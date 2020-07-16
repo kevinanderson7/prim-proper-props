c73b8d6a489dc64b6d9899d48b03de617872f584
Kevin Anderson <kmichael419@hotmail.com>
2020-07-16 15:18:07 -0500

    making Footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 class App extends Component {
   state = {
@@ -95,10 +96,7 @@ class App extends Component {
         <div>Spoons: {this.state.guestList.length * 2}</div>
         <div>Forks: {this.state.guestList.length * 2}</div>
         <div>Knives: {this.state.guestList.length * 2}</div>
-        <footer>
-          <h3>Have fun!</h3>
-          <p>Don't forget to mind your Ps and Qs!</p>
-        </footer>
+        <Footer />
       </div>
     );
   }
